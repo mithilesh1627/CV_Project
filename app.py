314325d06fbbb74cce0f7bf6c12ff6e7fb22cae7
Mithilesh <mithileshchaurasiya1627@gmail.com>
2025-04-10 10:28:02 +0530

    added the new webpage project_certificate

--- app.py
+++ app.py
@@ -5,17 +5,21 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-@app.route('/academic&technical')
+@app.route('/academic_technical')
 def academic():
     return render_template("academic_technical.html", title="Academic & Technical")
 
-@app.route('/projects')
+@app.route('/projects_certifications')
 def projects():
-    return render_template("projects.html")
+    return render_template("projects_certifications.html",title="Projects & Certifications")
 
 @app.route('/contact')
 def contact():
     return render_template("contact.html")
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
